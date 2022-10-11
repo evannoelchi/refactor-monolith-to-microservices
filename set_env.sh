af846c3f1b59c3b3cec2a62828240d117f2a67cf
evannoelchi <evannoelchi@gmail.com>
2022-10-11 16:37:04 +0200

    Breaking monolith app to microservices

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=dbpassword
-export POSTGRES_HOST=microserv-db.csbixftfermn.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=microservice-db.csbixftfermn.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=my-microservice-user2051861
 export AWS_REGION=us-east-1
